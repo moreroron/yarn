cf34923e4bd4d01075dc76be74b0ef43008e349b
moreroron <moreroron@gmail.com>
2019-06-10 17:23:42 +0300

    add review delete edit and index

--- yarn-rider/Models/Review.cs
+++ yarn-rider/Models/Review.cs
@@ -18,8 +18,7 @@ namespace yarn_rider.Models
         [Range(0, 10, ErrorMessage = "Rating must be between 0 and 10")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "A rate is required")]
         public int Rating { get; set; }
-
-        [Range(1990, 2019, ErrorMessage = "Date must be between 1990 and 2019")]
+        
         public string Date { get; set; }
 
         // Navigation Properties
